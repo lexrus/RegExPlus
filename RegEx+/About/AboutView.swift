c7c5b4597c99a7471402816d6ef749367eba920d
Lex Tang <lexrus@gmail.com>
2025-08-09 23:34:45 +0800

    feat: replace custom AboutView with AppAboutView package and add app icon

--- RegEx+/About/AboutView.swift
+++ RegEx+/About/AboutView.swift
@@ -8,93 +8,23 @@
 
 import SwiftUI
 import StoreKit
-
-
-private let kAppStoreReviewUrl = "https://apps.apple.com/app/regex/id1511763524?action=write-review"
-private let kGitHubUrl = "https://github.com/lexrus/RegExPlus"
-
+import AppAboutView
 
 struct AboutView: View {
-    @State private var showingAppStore = false
-    @State private var showingGitHub = false
-    @State private var showingAcknowledgements = false
-    
-    private var rateButton: some View {
-        Button(action: {
-            if #available(iOS 14.0, *) {
-                guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
-                    return
-                }
-                SKStoreReviewController.requestReview(in: scene)
-            } else {
-                SKStoreReviewController.requestReview()
-            }
-        }) {
-            Text("Rate RegEx+")
-        }
-    }
-    
-    private var appStoreButton: some View {
-        let url = URL(string: kAppStoreReviewUrl)!
-
-        return Button(action: {
-#if targetEnvironment(macCatalyst)
-            UIApplication.shared.open(url)
-#else
-            showingAppStore.toggle()
-#endif
-        }) {
-            Text("Write a review")
-        }
-        .sheet(isPresented: $showingAppStore) {
-             SafariView(url: url)
-        }
-    }
-    
-    private var gitHubButton: some View {
-        let url = URL(string: kGitHubUrl)!
-
-        return Button(action: {
-#if targetEnvironment(macCatalyst)
-            UIApplication.shared.open(url)
-#else
-            showingGitHub.toggle()
-#endif
-        }) {
-            Text(kGitHubUrl)
-        }
-        .sheet(isPresented: $showingGitHub) {
-            SafariView(url: url)
-        }
-    }
-
     var body: some View {
-        List {
-            Section(header: Text("\(Bundle.main.releaseVersionNumber ?? "")")) {
-                rateButton
-                appStoreButton
-                gitHubButton
-            }
-        }
-        .listStyle(InsetGroupedListStyle())
+        AppAboutView.fromMainBundle(
+          appIcon: Image(.appIconForAboutView),
+          feedbackEmail: "lexrus@gmail.com",
+          appStoreID: "1511763524",
+          privacyPolicy: URL(string: "https://lex.sh/regexplus/privacypolicy")!,
+          copyrightText: "©2025 lex.sh"
+        )
+        .background(Color.init(white: 0.5, opacity: 0.1))
+        .navigationBarTitleDisplayMode(.inline)
         .navigationBarTitle("RegEx+")
     }
 }
 
-extension Bundle {
-    
-    var releaseVersionNumber: String? {
-        return infoDictionary?["CFBundleShortVersionString"] as? String
-    }
-    
-    var buildVersionNumber: String? {
-        return infoDictionary?["CFBundleVersion"] as? String
-    }
-    
-}
-
-struct AboutView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutView()
-    }
+#Preview {
+    AboutView()
 }
